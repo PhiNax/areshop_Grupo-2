fe03939ed0e006ce473a84d5b3003b8218f630dd
Ignacio <ignaciolmartin@gmail.com>
2021-11-19 01:03:20 -0300

    Changed usersController
        - Added user model to connect to the database
        - Edited login and register controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,54 +1,39 @@
-// Call FileSystem module
-const fs = require('fs');
-// Call Path module
-const path = require('path');
-
 // Call result validation errors
 const { validationResult } = require('express-validator');
 // Call Bcrypt for encrypt passwords
 const bcrypt = require('bcryptjs');
-const { v4: uuidv4 } = require('uuid');
-
-const usersFilePath = path.join(__dirname, '../database/users.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+// Call User Model 
+const { User } = require('../database/connectDB');
 
 const controller = {
     // Cart
     cart: (req, res) => {
         res.render('users/cart');
     },
-
-
     // Register render register form
     register: (req, res) => {
         res.render('users/register');
     },
-
     // Store New User on DataBase
     createUser: async (req, res) => {
         const errors = validationResult(req);
 
         if (errors.isEmpty()) {
+
             let passCrypt = await bcrypt.hash(req.body.password, 10);
 
             let newUser = {
-                id: uuidv4(),
                 name: req.body.name,
                 nickname: req.body.nickname,
                 email: req.body.email,
                 password: passCrypt,
-                userImage: req.file.filename,
             }
-            console.log(newUser);
             try {
-                users.push(newUser);
-                fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 4), { encoding: "utf-8" });
-                req.session.userLogged = newUser.email;
-                res.redirect('/');
+                await User.create(newUser);
+                res.redirect('/login');
             }
             catch (err) {
                 throw new Error('Create New User: Error => ' + err);
-
             }
         } else {
             res.render('users/register', { errors: errors.mapped(), old: req.body });
@@ -58,49 +43,60 @@ const controller = {
     login: (req, res) => {
         res.render('users/login');
     },
-    loginProcess: (req, res) => {
-        let userToLogin = users.find(user => user.email == req.body.email);
+    //  Login process
+    loginProcess: async (req, res) => {
+        const errors = validationResult(req);
 
-        if (userToLogin) {
-            let isOkThePassword = bcrypt.compareSync(req.body.password, userToLogin.password);
-            if (isOkThePassword) {
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
+        if (errors.isEmpty()) {
 
-                if (req.body.remember_user == "on") {
-                    res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 200 })
-                }
-                return res.redirect('/profile');
-            }
-            return res.render('users/login', {
-                errors: {
-                    email: {
-                        msg: 'Las credenciales son inválidas'
+            const email = req.body.email;
+            const password = req.body.password;
+            // remember req.session.cookie.maxAge = 60000 * 600 * 24 * 7;
+
+            try {
+                const userLogin = await User.findOne({
+                    where: {
+                        email: email
                     }
-                }
-            });
-        }
+                });
+                if (userLogin && await bcrypt.compare(password, userLogin.password)) {
 
-        return res.render('users/login', {
-            errors: {
-                email: {
-                    msg: 'Las credenciales son inválidas'
+                    req.session.userLogged = {
+                        name: userLogin.name,
+                        email: userLogin.email,
+                        avatar: userLogin.avatar
+                    };
+                    res.redirect('/');
+                } else {
+                    const errors = {
+                        credentials: {
+                            msg: 'Credentials do not match'
+                        }
+                    };
+                    res.render('users/login', { errors: errors, old: req.body });
                 }
+            }
+            catch (err) {
+                throw new Error('Login User: Error => ' + err);
+            }
+        } else {
+            res.render('users/login', { errors: errors.mapped(), old: req.body });
+        }
+    },
+    // Log out user and destroy the session information
+    logout: (req, res) => {
+        req.session.destroy(function (err) {
+            if (err) {
+                throw new Error('Can not log out User: Error => ' + err);
             }
         });
+        res.clearCookie('connect.sid').redirect('/');
     },
     profile: (req, res) => {
-        return res.render('userProfile', {
+        return res.render('users/userProfile', {
             user: req.session.userLogged
         });
 
-    },
-
-    // Log out user and destroy the session information
-    logout: (req, res) => {
-        res.clearCookie('connect.sid');
-        req.session.destroy();
-        return res.redirect('/login');
     }
 };
 
