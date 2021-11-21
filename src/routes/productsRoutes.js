1dcc1db39500ef97163778177707ed061e1010cf
Ignacio <ignaciolmartin@gmail.com>
2021-11-21 00:22:49 -0300

    Added multerGameImage middleware file for upload games cover images

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,26 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
-
+// Call products controller
 const productsController = require('../controllers/productsController');
 
-const path = require("path");
-
-// Call Multer for image storage module
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/img/products/'));
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e13) + '-' + file.originalname;
-        cb(null, file.fieldname + '-' + uniqueSuffix);
-    },
-});
-
-const upload = multer({ storage: storage });
+// Call multer middleware to upload games images cover
+const uploadCoverImage = require('../middleware/multerGameImage');
 
 // Set Routes
 // Product Details Route
@@ -30,14 +14,14 @@ router.get('/', productsController.list);
 router.get('/create', productsController.create);
 
 // Product Catch Data from Create Form Route
-router.post('/store', upload.single("image"), productsController.store);
+router.post('/store', uploadCoverImage.single("coverImage"), productsController.store);
 
 // Product Details Route
 router.get('/:id', productsController.detail);
 
 // Product Edit
 router.get("/edit/:id", productsController.getEdit)
-router.put('/:id', upload.single("image"), productsController.edit)
+router.put('/:id', uploadCoverImage.single("image"), productsController.edit)
 
 // Product Delete
 router.delete('/:id', productsController.delete)
