e20555d18d088d804dd29ae35833a97b55e7306a
Ignacio <ignaciolmartin@gmail.com>
2022-01-05 22:05:18 -0300

    Update v2.0

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -6,4 +6,10 @@ const apiController = require('../controllers/apiController');
 // Set API Routes
 router.get('/users', apiController.users);
 
+router.get('/users/:id', apiController.userId);
+
+router.get('/products', apiController.products);
+
+router.get('/products/:id', apiController.productId)
+
 module.exports = router;
