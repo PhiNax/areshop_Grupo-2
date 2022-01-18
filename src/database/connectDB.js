723d9fa6b450e20ee7523d2f86f24cee267c36ef
Ignacio <ignaciolmartin@gmail.com>
2022-01-17 21:46:34 -0300

    Fixes

--- src/database/connectDB.js
+++ src/database/connectDB.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(CLEARDB_DATABASE_URL,
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL,
     {
         dialect: 'mysql'
     })
