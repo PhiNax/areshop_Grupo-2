8397065e09b4d8e3e236b2cdd9e2952711177bfd
Ignacio <ignaciolmartin@gmail.com>
2022-01-17 21:45:28 -0300

    Fixes

--- src/database/connectDB.js
+++ src/database/connectDB.js
@@ -1,16 +1,13 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+const sequelize = new Sequelize(CLEARDB_DATABASE_URL,
     {
-        host: process.env.DB_HOST,
         dialect: 'mysql'
     })
 
 try {
     sequelize.authenticate()
+
     console.log('Connection has been established successfully.');
 }
 catch (err) {
